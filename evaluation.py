26fbe2790201ec927de3249af69d328d116b8fb7
Matteo <ma.scrugli@gmail.com>
2022-05-06 16:33:26 +0200

    roc curve

--- evaluation.py
+++ evaluation.py
@@ -596,6 +596,42 @@ time.sleep(0.1)
 
 
 
+# # ██████╗  ██████╗  ██████╗
+# # ██╔══██╗██╔═══██╗██╔════╝
+# # ██████╔╝██║   ██║██║
+# # ██╔══██╗██║   ██║██║
+# # ██║  ██║╚██████╔╝╚██████╗
+# # ╚═╝  ╚═╝ ╚═════╝  ╚═════╝
+#
+# import sklearn.metrics as metrics
+# # calculate the fpr and tpr for all thresholds of the classification
+# probs = model_quantized.predict_proba(X_test)
+# preds = probs[:,1]
+# fpr, tpr, threshold = metrics.roc_curve(Y_test, preds)
+# roc_auc = metrics.auc(fpr, tpr)
+#
+# # method I: plt
+# import matplotlib.pyplot as plt
+# plt.title('Receiver Operating Characteristic')
+# plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+# plt.legend(loc = 'lower right')
+# plt.plot([0, 1], [0, 1],'r--')
+# plt.xlim([0, 1])
+# plt.ylim([0, 1])
+# plt.ylabel('True Positive Rate')
+# plt.xlabel('False Positive Rate')
+# # plt.show()
+#
+# # exit()
+
+
+
+
+
+
+
+
+
 # ██████╗ ███████╗ █████╗ ██╗  ██╗    ███████╗██╗   ██╗ █████╗ ██╗     ██╗   ██╗ █████╗ ████████╗██╗ ██████╗ ███╗   ██╗
 # ██╔══██╗██╔════╝██╔══██╗██║ ██╔╝    ██╔════╝██║   ██║██╔══██╗██║     ██║   ██║██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║
 # ██████╔╝█████╗  ███████║█████╔╝     █████╗  ██║   ██║███████║██║     ██║   ██║███████║   ██║   ██║██║   ██║██╔██╗ ██║
